8462ebeb51c4a4c470c01c1b43bce5ce8f4e51e5
user3 <user3@example.com>
2022-06-11 10:30:54 +0200

    Добавьте файлы проекта.

---
+++ Laba1.BAse/Result.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba1.BAse
+{
+    internal class Result
+    {
+        public double A { get; set; }
+        public double B { get; set; }
+        public Result(double a, double b)
+        {
+            A = a;
+            B = b;
+        }
+        public double Otvet()
+        {
+            return A * B;
+        }
+    }
+}
